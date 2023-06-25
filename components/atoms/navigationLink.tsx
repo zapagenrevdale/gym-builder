ed9c518d73347009ddd0aac0b164398677c1d206
zapagenrevdale <zapagenrevdale@gmail.com>
2023-06-25 16:53:51 +0800

    Add navigation link hover effects

--- components/atoms/navigationLink.tsx
+++ components/atoms/navigationLink.tsx
@@ -1,9 +1,25 @@
 import Link from "next/link";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 const NavigationLink: React.FC<Props> = ({ href, text }) => {
+  const [hovered, setHovered] = useState(false);
+
   return (
-    <Link href={href} className="font-medium text-lg font-grotesk">
-      {text}
+    <Link
+      href={href}
+      className="font-medium text-lg font-grotesk"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <p>{text}</p>
+      <motion.div
+        animate={{
+          width: hovered ? "100%" : "0px",
+          transition: { duration: 0.3, ease: "easeInOut" },
+        }}
+        className="bg-neutral-800 h-[2px]"
+      />
     </Link>
   );
 };
